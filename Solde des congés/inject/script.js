1c6bed328b11044eee2bd0fc0b7148afd003b2a4
Antoine Duval <33424294+HeyHeyChicken@users.noreply.github.com>
2022-07-12 14:32:48 +0200

    Debug & Add total

--- Solde des congés/inject/script.js
+++ Solde des congés/inject/script.js
@@ -41,13 +41,23 @@ function start(){
   }
 }
 
+function GetPremierTableauTR(_nom){
+  for(let x = 1; x < premier_tableau_trs.length; x++){
+    if(premier_tableau_trs[x].querySelectorAll("td")[0].innerHTML == _nom){
+      return premier_tableau_trs[x];
+    }
+  }
+}
+
 // Cette fonction répercute les jours "RTT" du deuxième tableau au premier.
 function DoRTT(_salaryMode, _value, _statut, _tds){
-  const TD1 = premier_tableau_trs[_salaryMode ? 8 : 7].querySelectorAll("td")[3];
+  const TR = _salaryMode ? GetPremierTableauTR("RTT Salarié") : GetPremierTableauTR("RTT Employeur");
+  const TD1 = TR.querySelectorAll("td")[2];
   SetTDValue(TD1, (TD1.innerHTML - _value));
+  const TD2 = TR.querySelectorAll("td")[3];
+  SetTDValue(TD2, (TD2.innerHTML - _value));
 
   if(_statut.includes("Demande non encore validée ou pointée")){
-    const TD2 = premier_tableau_trs[_salaryMode ? 8 : 7].querySelectorAll("td")[2];
     SetTDValue(TD2, (TD2.innerHTML - _value));
   }
 
@@ -56,17 +66,21 @@ function DoRTT(_salaryMode, _value, _statut, _tds){
 
 // Cette fonction répercute les jours "Congés payés" du deuxième tableau au premier.
 function DoCP(_statutValideOuPointe, _value){
-  const TD1 = premier_tableau_trs[3].querySelectorAll("td")[_statutValideOuPointe ? 3 : 2];
+  const TD1 = GetPremierTableauTR("Congés Payés 2").querySelectorAll("td")[_statutValideOuPointe ? 2 : 3];
   let value1 = parseFloat((TD1.innerHTML));
   if(value1 - _value < 0){
     const VALUE1bis = value1 - _value;
     value1 = 0;
 
-    const TD1bis = premier_tableau_trs[2].querySelectorAll("td")[_statutValideOuPointe ? 3 : 2];
+    const TD1bis = GetPremierTableauTR("Congés Payés 1").querySelectorAll("td")[_statutValideOuPointe ? 2 : 3];
     const TD1bisValue = parseFloat(TD1bis.innerHTML);
     SetTDValue(TD1bis, (TD1bisValue + VALUE1bis));
   }
   SetTDValue(TD1, (value1 - _value >= 0) ? value1 - _value : 0);
+  if(_statutValideOuPointe){
+    const TD2 = GetPremierTableauTR("Congés Payés 2").querySelectorAll("td")[3];
+    SetTDValue(TD2, (value1 - _value >= 0) ? value1 - _value : 0);
+  }
 }
 
 // Cette fonction définit le contenu d'une cellule.
@@ -94,8 +108,19 @@ function dev_launch(){
 
 function launch(){
   premier_tableau = document.querySelector("#SoldeCongePerso > table > tbody");
-  premier_tableau_trs = premier_tableau.querySelectorAll("tr");
   deuxieme_tableau = document.querySelector("#SoldesAbsencesPerso > tbody");
+
+  // On ajoute une ligne "total" au tableau
+  const TR = document.createElement("tr");
+  const TH = document.createElement("td");
+  const TD = document.createElement("td");
+  TR.setAttribute("id", "total");
+  TH.appendChild(document.createTextNode("Total"));
+  TD.appendChild(document.createTextNode("0"));
+  TR.appendChild(TH);
+  TR.appendChild(TD);
+  premier_tableau.appendChild(TR);
+  premier_tableau_trs = premier_tableau.querySelectorAll("tr");
   deuxieme_tableau_trs = deuxieme_tableau.querySelectorAll("tr");
 
   // Dans cette boucle, nous ajoutons les colonnes vide
@@ -142,13 +167,26 @@ function launch(){
       SECOND_CONTAINER.setAttribute("title", "Cette colonne contient les congés du dernier bulletin de paie, moins les absences validées ou pointées et non encore validées ou pointées.");
     }
     SECOND_CONTAINER.appendChild(document.createTextNode(i == 0 ? "Non encore validés ou pointés (?)" : value));
+    if(premier_tableau_trs[i] != GetPremierTableauTR("Congés Payés 1") && premier_tableau_trs[i] != GetPremierTableauTR("Congés Payés 2")){
+      SECOND_CONTAINER.setAttribute("colspan", "2");
+    }
     premier_tableau_trs[i].appendChild(SECOND_CONTAINER);
 
     // Troisième colonne "Date"
-    const TROISIEME_CONTAINER = document.createElement(i == 0 ? "th" : "td");
-    TROISIEME_CONTAINER.classList.add(EXTENSION_ADDED_ATTRIBUTE);
+    if(premier_tableau_trs[i] == GetPremierTableauTR("Congés Payés 1")){
+      const TROISIEME_CONTAINER = document.createElement(i == 0 ? "th" : "td");
+      TROISIEME_CONTAINER.classList.add(EXTENSION_ADDED_ATTRIBUTE);
+      TROISIEME_CONTAINER.appendChild(document.createTextNode(""));
+      TROISIEME_CONTAINER.setAttribute("rowspan", "2");
+      TROISIEME_CONTAINER.setAttribute("id", "final_conges");
+      premier_tableau_trs[i].appendChild(TROISIEME_CONTAINER);
+    }
+
+    // Quatrième colonne "Date"
+    const QUATRIEME_CONTAINER = document.createElement(i == 0 ? "th" : "td");
+    QUATRIEME_CONTAINER.classList.add(EXTENSION_ADDED_ATTRIBUTE);
     if(i == 0){
-      TROISIEME_CONTAINER.setAttribute("title", "Cette colonne contient les congés estimés à la date sélectionnée par l'utilisateur.");
+      QUATRIEME_CONTAINER.setAttribute("title", "Cette colonne contient les congés estimés à la date sélectionnée par l'utilisateur.");
 
       const INPUT = document.createElement("input");
       INPUT.setAttribute("type", "month");
@@ -159,12 +197,12 @@ function launch(){
       }
       INPUT.classList.add(EXTENSION_ADDED_ATTRIBUTE, "futur");
 
-      TROISIEME_CONTAINER.appendChild(INPUT);
+      QUATRIEME_CONTAINER.appendChild(INPUT);
     }
     else{
-      TROISIEME_CONTAINER.appendChild(document.createTextNode(""));
+      QUATRIEME_CONTAINER.appendChild(document.createTextNode(""));
     }
-    //premier_tableau_trs[i].appendChild(TROISIEME_CONTAINER);
+    //premier_tableau_trs[i].appendChild(QUATRIEME_CONTAINER);
   }
 
   for(let i = 1; i < deuxieme_tableau_trs.length; i++){
@@ -194,6 +232,24 @@ function launch(){
 
       TDS[4].classList.add(DONE_ATTRIBUTE);
     }
+  }
+
+  // On aditionne les dernieres valeurs des congés pour les additionner.
+  SetTDValue(
+    document.getElementById("final_conges"),
+    (parseFloat(GetPremierTableauTR("Congés Payés 1").querySelectorAll("td")[3].innerHTML) + parseFloat(GetPremierTableauTR("Congés Payés 2").querySelectorAll("td")[3].innerHTML))
+  );
+
+  // On s'occupe d'additionner les totaux
+  for(let x = 1; x < GetPremierTableauTR("Total").querySelectorAll("td").length; x++){
+    let total = 0;
+    for(let y = 1; y < premier_tableau_trs.length - 1; y++){
+      total += parseFloat(premier_tableau_trs[y].querySelectorAll("td")[x].innerHTML.replace(/,/g, '.'));
+    }
+    SetTDValue(
+      GetPremierTableauTR("Total").querySelectorAll("td")[x],
+      total
+    );
   }
 
   // S'il reste des lignes dans le deuxième tableau qui sont non traitées, on ajoute un message d'avertissement.
@@ -240,7 +296,6 @@ function EstimateToDate(){
   if(1 == 1 /* && DATE > last_day */ && DATE >= next_date){
     INPUT.classList.remove("red");
 
-    console.log(next_date);
     while(current_date < DATE){
       for(let i = 1; i < premier_tableau_trs.length; i++){
         const TDS = premier_tableau_trs[i].querySelectorAll("td");
